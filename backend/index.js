51a6d3e5cb179d7ecf100de8dee3040338f8bc41
Emeline GARO <emeline.garo@supinternet.fr>
2022-03-20 22:16:24 +0100

    wip

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 3001;
-const HOST = '127.0.0.1';
+const PORT = 27017;
+const HOST = 'localhost';
 const app = express();
-var cors = require('cors')
-app.use(cors())
+var cors = require('cors');
+app.use(cors());
 
 app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-
 
 // app.get('/', (req, res) => {
 //   db.collection("data").find({}).toArray(function (err, result) {
@@ -24,44 +22,40 @@ app.use(express.static(__dirname)); //here is important thing - no static direct
 //   })
 // });
 
-
 const { MongoClient } = require('mongodb');
-const connection = "mongodb://localhost:27017";
+const connection = 'mongodb://localhost:27017';
 const client = new MongoClient(connection);
-
-
 
 app.get('/city', async (req, res) => {
   const co = client.connect();
-  const db = client.db("green-it");
-  const testCollection = db.collection("data");
-  // Il faut créer l'index pour dire a mongo qu'on peut chercher dedans 
-  const indexResult = testCollection.createIndex({ Libcom: 'text' })
+  const db = client.db('green-it');
+  const testCollection = db.collection('data');
+  // Il faut créer l'index pour dire a mongo qu'on peut chercher dedans
+  const indexResult = testCollection.createIndex({ Libcom: 'text' });
   // la query avec le parametre city?=laville
   const query = { $text: { $search: req.query.q } };
-  // On a besoins pour avoir les champs qu'on veut dans le resultat de la requete 
-  const projection = { 
-    Libcom: 1, 
-    Libreg:1, Libdep :1 , 
-    "SCORE GLOBAL departement 1" :1 ,
-    "SCORE GLOBAL region 1": 1 , 
-    "ACCÈS AUX INTERFACES NUMERIQUES": 1, 
+  // On a besoins pour avoir les champs qu'on veut dans le resultat de la requete
+  const projection = {
+    Libcom: 1,
+    Libreg: 1,
+    Libdep: 1,
+    'SCORE GLOBAL departement 1': 1,
+    'SCORE GLOBAL region 1': 1,
+    'ACCÈS AUX INTERFACES NUMERIQUES': 1,
     "ACCES A L'INFORMATION": 1,
-    "COMPETENCES ADMINISTATIVES": 1,
-    "COMPÉTENCES NUMÉRIQUES / SCOLAIRES": 1
+    'COMPETENCES ADMINISTATIVES': 1,
+    'COMPÉTENCES NUMÉRIQUES / SCOLAIRES': 1,
   };
-  // Va chercher le resultat de la query avec les champs qu'on veut 
-  const cursor = testCollection.find(query).project(projection); 
-  const allVal = await cursor.toArray(); 
-  console.log(typeof allVal)
+  // Va chercher le resultat de la query avec les champs qu'on veut
+  const cursor = testCollection.find(query).project(projection);
+  const allVal = await cursor.toArray();
+  console.log(typeof allVal);
 
   // console.log(allVal);
-  res.send(JSON.stringify(allVal)) 
-})
-
+  res.send(JSON.stringify(allVal));
+});
 
 // Connection URL
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
--- front-react/src/Components/search.js
+++ front-react/src/Components/search.js
@@ -1,48 +1,39 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
 
 function SearchBar(props) {
+  const [searchInput, setSearchInput] = useState('');
 
-    const [searchInput, setSearchInput] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+    console.log(e);
+  };
 
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-        console.log(e)
-    };
-
-    const getInfos = () => {
-      if (searchInput.length > 0) {
-        fetch(`http://127.0.0.1:3000/city?q=${searchInput}`, {
-          method:"GET"
-        })
-        .then(response => console.log(response.json()))
-        // .then(response => console.log(response.body))
-      }
-  
+  const getInfos = () => {
+    if (searchInput.length > 0) {
+      fetch(`http://127.0.0.1:3000/city?q=${searchInput}`, {
+        method: 'GET',
+      }).then((response) => console.log(response.json()));
+      // .then(response => console.log(response.body))
     }
-    
+  };
 
+  return (
+    <div id='search-div'>
+      <input
+        onChange={handleChange}
+        type='search'
+        placeholder='Cherchez ici'
+        value={searchInput}
+      />
 
-  return(
-  <div id="search-div"> 
-
-    <input
-      onChange={handleChange}
-       type="search"
-       placeholder="Cherchez ici"
-       value={searchInput} />
-
-
-
-       <button  onClick={getInfos} type='submit'>
-       Rechercher
-       </button>
-{/*     <table>
+      <button onClick={getInfos} type='submit'>
+        Rechercher
+      </button>
+      {/*     <table>
       <tr>
         <th>Commune</th>
-        <th>Score global</th>  
+        <th>Score global</th>
       </tr>
     {Object.values(data_city).map((city, index) => (
     <div>
@@ -54,7 +45,7 @@ function SearchBar(props) {
     ))}
     </table> */}
     </div>
-   );
+  );
 }
 
 export default SearchBar;
